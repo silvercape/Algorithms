83f5c091594d99f4c464e13a611f532498f190d3
“silvercape” <1028280371@qq.com>
2023-02-06 18:13:00 +0800

    add joseph math solution

---
+++ 基础数据结构/链表/约瑟夫问题/math.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 自制力 on 2023/2/6.
+// 洛谷 P1996
+// In
+// 10 3
+// Out
+// 3 6 9 2 7 1 8 5 10 4
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int recur(int n, int m) {
+    if (n == 1) {
+        return 1;
+    }
+
+    return (recur(n - 1, m) + m - 1) % n + 1;
+
+}
+
+int main() {
+    // get people number and out number
+    int n, m;
+    cin >> n >> m;
+
+    cout << recur(n, m) << endl;
+    return 0;
+}
+
